0c762fb14f2337ef86e3c737a21d047489a8b9d5
Tanguy Pinel <tanguypinel@gmail.com>
2022-08-21 19:38:29 +0200

    Initv2

--- wp-content/themes/tanguyp-child/footer.php
+++ wp-content/themes/tanguyp-child/footer.php
@@ -10,23 +10,24 @@
  */
 
 ?>
+<footer id="colophon" class="site-footer">
+	<div class="site-info">
+		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tanguyp' ) ); ?>">
+			<?php
+			/* translators: %s: CMS name, i.e. WordPress. */
+			printf( esc_html__( 'Proudly powered by %s', 'tanguyp' ), 'WordPress' );
+			?>
+		</a>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tanguyp' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'tanguyp' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'tanguyp' ), 'tanguyp', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<span class="sep"> | </span>
+		<?php
+		/* translators: 1: Theme name, 2: Theme author. */
+		printf( esc_html__( 'Theme: %1$s by %2$s.', 'tanguyp' ), 'tanguyp', '<a href="http://underscores.me/">Underscores.me</a>' );
+		?>
+	</div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
+
 
 <?php wp_footer(); ?>
 
--- wp-content/themes/tanguyp-child/header.php
+++ wp-content/themes/tanguyp-child/header.php
@@ -11,46 +11,46 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 
-	<?php wp_head(); ?>
-</head>
+		<?php wp_head(); ?>
+	</head>
 
-<body <?php body_class(); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tanguyp' ); ?></a>
+	<body <?php body_class(); ?>>
+		<?php wp_body_open(); ?>
+		<div id="page" class="site">
+			<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'tanguyp' ); ?></a>
 
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
+			<header id="masthead" class="site-header">
+				<div class="site-branding">
+					<?php
+					the_custom_logo();
+					if ( is_front_page() && is_home() ) :
+						?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><p class="site-title"></p></a>
+					<?php
+				endif;
 				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+
+			</div><!-- .site-branding -->
+
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tanguyp' ); ?></button>
 				<?php
-			else :
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
 				?>
-				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><p class="site-title"></p></a>
-				<?php
-			endif;
-				?>
-
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'tanguyp' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
+			</nav><!-- #site-navigation -->
+		</header><!-- #masthead -->
